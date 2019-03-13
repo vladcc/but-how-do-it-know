933980b981bcc00ef2356d5cac104154bd0692c9
Vladimir Dinev <vld.dinev@gmail.com>
2019-03-13 22:18:08 +0200

    Formatting and whitespace issue in os_def.h fixed.

--- jcp/os_def.h
+++ jcp/os_def.h
@@ -1,11 +1,11 @@
 /* os_def.h -- define if you want to compile for Windows or Linux here */
 #ifndef OS_DEF_H
 #define OS_DEF_H
-// #define WINDOWS / LINUX
+
 #ifdef _WIN32
-# define WINDOWS
+	#define WINDOWS
 #elif defined (__linux__)
-# define LINUX
+	#define LINUX
 #endif
 
 #ifdef WINDOWS
